fd11be9f4b1c6d61f994d205beed3514202f000f
Hoang Phuc <phucnguyen692004@gmail.com>
2024-01-17 23:46:51 +0700

    nodejs

--- src/migrations/migration-create-allcode.js
+++ src/migrations/migration-create-allcode.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
     up: async (queryInterface, Sequelize) => {
-        await queryInterface.createTable('allcode', {
+        await queryInterface.createTable('allcodes', {
             id: {
                 allowNull: false,
                 autoIncrement: true,
@@ -20,7 +20,7 @@ module.exports = {
             valueVi: {
                 type: Sequelize.STRING
             },
-           
+
 
             createdAt: {
                 allowNull: false,
@@ -33,6 +33,6 @@ module.exports = {
         });
     },
     down: async (queryInterface, Sequelize) => {
-        await queryInterface.dropTable('allcode');
+        await queryInterface.dropTable('allcodes');
     }
 };
